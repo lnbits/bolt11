8d22414591e01c2e6cf0e33fd9939f3cd9562caf
michael1011 <me@michael1011.at>
2023-08-12 14:36:14 +0200

    [FIX] add missing exports (#29)

--- bolt11/__init__.py
+++ bolt11/__init__.py
@@ -6,5 +6,7 @@ from .encode import encode
 from .models.fallback import Fallback
 from .models.features import Feature, Features, FeatureState
 from .models.routehint import Route, RouteHint
-from .types import Bolt11
+from .models.signature import Signature
+from .models.tags import Tag, Tags, TagChar
+from .types import Bolt11, MilliSatoshi
 from .utils import amount_to_btc, btc_to_amount
